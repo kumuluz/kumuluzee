cd4530671f6fa29ec59d7793f3bb37003cb433b1
Tilen Faganel <tilen.faganel@me.com>
2015-05-04 13:37:06 +0200

    Added core module

---
+++ core/src/test/java/com/kumuluz/ee/test/ExampleTest.java
@@ -0,0 +1,16 @@
+package com.kumuluz.ee.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Tilen Faganel
+ */
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+
+        Assert.assertEquals(1 + 2, 3);
+    }
+}
