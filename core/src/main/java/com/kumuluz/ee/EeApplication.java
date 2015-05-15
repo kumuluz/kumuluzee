101d4d4590f585dc253274182c3d95b33e24628e
Tilen Faganel <tilen.faganel@me.com>
2015-05-15 02:33:17 +0200

    Added main application class

---
+++ core/src/main/java/com/kumuluz/ee/EeApplication.java
@@ -0,0 +1,20 @@
+package com.kumuluz.ee;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Tilen Faganel
+ * @since 1.0.0
+ */
+public class EeApplication {
+
+    private Logger log = Logger.getLogger(EeApplication.class.getSimpleName());
+
+    public EeApplication() {
+
+    }
+
+    public void initialize() {
+
+    }
+}
