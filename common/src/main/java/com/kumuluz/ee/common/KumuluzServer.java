57f3f22905973f2a2e26d0a571bca8e5f44a78ed
Tilen Faganel <tilen.faganel@me.com>
2015-05-24 22:37:27 +0200

    Updated java ee specification dependencies

--- common/src/main/java/com/kumuluz/ee/common/KumuluzServer.java
+++ common/src/main/java/com/kumuluz/ee/common/KumuluzServer.java
@@ -2,14 +2,16 @@ package com.kumuluz.ee.common;
 
 import java.util.Map;
 
+import javax.servlet.Servlet;
+
 /**
  * @author Tilen
  */
 public interface KumuluzServer {
 
-    void registerServlet(Class<?> servletClass, String mapping);
+    void registerServlet(Class<? extends Servlet> servletClass, String mapping);
 
-    void registerServlet(Class<?> servletClass, String mapping, Map<String, String> parameters);
+    void registerServlet(Class<? extends Servlet> servletClass, String mapping, Map<String, String> parameters);
 
     String getServerName();
 }
