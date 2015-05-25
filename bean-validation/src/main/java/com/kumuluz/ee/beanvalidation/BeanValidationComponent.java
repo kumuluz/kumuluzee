bb231b9ee0806ce45277558456168c11f3e175fe
Tilen Faganel <tilen.faganel@me.com>
2015-05-25 09:10:23 +0200

    Added bean validation component

---
+++ bean-validation/src/main/java/com/kumuluz/ee/beanvalidation/BeanValidationComponent.java
@@ -0,0 +1,36 @@
+package com.kumuluz.ee.beanvalidation;
+
+import com.kumuluz.ee.common.Component;
+import com.kumuluz.ee.common.KumuluzServer;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Tilen
+ */
+public class BeanValidationComponent implements Component {
+
+    private Logger log = Logger.getLogger(BeanValidationComponent.class.getSimpleName());
+
+    @Override
+    public void init(KumuluzServer server) {
+    }
+
+    @Override
+    public void load() {
+        
+        log.info("Initiating Hibernate Validator");
+    }
+
+    @Override
+    public String getComponentName() {
+
+        return "Bean Validation";
+    }
+
+    @Override
+    public String getImplementationName() {
+
+        return "Hibernate Validator";
+    }
+}
