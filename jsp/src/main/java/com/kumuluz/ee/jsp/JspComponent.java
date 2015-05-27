0c13752dbc73cd08ea5d9bbda0305a961e3208db
Tilen Faganel <tilen.faganel@me.com>
2015-05-27 07:13:43 +0200

    Added missing jsp component

---
+++ jsp/src/main/java/com/kumuluz/ee/jsp/JspComponent.java
@@ -0,0 +1,37 @@
+package com.kumuluz.ee.jsp;
+
+import com.kumuluz.ee.common.Component;
+import com.kumuluz.ee.common.KumuluzServer;
+import com.kumuluz.ee.common.config.EeConfig;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Tilen
+ */
+public class JspComponent implements Component {
+
+    private Logger log = Logger.getLogger(JspComponent.class.getSimpleName());
+
+    @Override
+    public void init(KumuluzServer server, EeConfig eeConfig) {
+    }
+
+    @Override
+    public void load() {
+
+        log.info("Initiating JSP");
+    }
+
+    @Override
+    public String getComponentName() {
+
+        return "JSP";
+    }
+
+    @Override
+    public String getImplementationName() {
+
+        return "JSP";
+    }
+}
