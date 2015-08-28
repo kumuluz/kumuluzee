2b9eb5732ec7e30d7960cb1e0f997226de665b00
Tilen Faganel <tilen.faganel@me.com>
2015-08-28 02:52:41 +0200

    Added EL base component initialization class and service loader config

---
+++ el/src/main/java/com/kumuluz/ee/el/ElComponent.java
@@ -0,0 +1,37 @@
+package com.kumuluz.ee.el;
+
+import com.kumuluz.ee.common.Component;
+import com.kumuluz.ee.common.KumuluzServer;
+import com.kumuluz.ee.common.config.EeConfig;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Tilen
+ */
+public class ElComponent implements Component {
+
+    private Logger log = Logger.getLogger(ElComponent.class.getSimpleName());
+
+    @Override
+    public void init(KumuluzServer server, EeConfig eeConfig) {
+    }
+
+    @Override
+    public void load() {
+
+        log.info("Initiating UEL");
+    }
+
+    @Override
+    public String getComponentName() {
+
+        return "EL";
+    }
+
+    @Override
+    public String getImplementationName() {
+
+        return "UEL";
+    }
+}
