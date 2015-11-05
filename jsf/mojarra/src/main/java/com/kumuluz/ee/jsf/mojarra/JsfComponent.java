8a4098b14589f96a17a3109dd1489eb449135ff9
Tilen Faganel <tilen.faganel@me.com>
2015-11-05 13:35:04 -0800

    Added experimental mojarra component with CDI enabled

---
+++ jsf/mojarra/src/main/java/com/kumuluz/ee/jsf/mojarra/JsfComponent.java
@@ -0,0 +1,37 @@
+package com.kumuluz.ee.jsf.mojarra;
+
+import com.kumuluz.ee.common.Component;
+import com.kumuluz.ee.common.KumuluzServer;
+import com.kumuluz.ee.common.config.EeConfig;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Tilen
+ */
+public class JsfComponent implements Component {
+
+    private Logger log = Logger.getLogger(JsfComponent.class.getSimpleName());
+
+    @Override
+    public void init(KumuluzServer server, EeConfig eeConfig) {
+    }
+
+    @Override
+    public void load() {
+
+        log.info("Initiating Mojarra");
+    }
+
+    @Override
+    public String getComponentName() {
+
+        return "JSF";
+    }
+
+    @Override
+    public String getImplementationName() {
+
+        return "Mojarra";
+    }
+}
