193eab078dbdf821bc390ff844e0f7e02480b855
Tilen Faganel <tilen.faganel@me.com>
2016-03-10 02:14:59 +0100

    Temporary added JAX-WS and EJB skeletons for testing purposes.

---
+++ components/jax-ws/metro/src/main/java/com/kumuluz/ee/jaxws/metro/JaxWsComponent.java
@@ -0,0 +1,36 @@
+package com.kumuluz.ee.jaxws.metro;
+
+import com.kumuluz.ee.common.Component;
+import com.kumuluz.ee.common.KumuluzServer;
+import com.kumuluz.ee.common.config.EeConfig;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Tilen Faganel
+ * @since 2.0.0
+ */
+public class JaxWsComponent implements Component {
+
+    private Logger log = Logger.getLogger(JaxWsComponent.class.getSimpleName());
+
+    @Override
+    public void init(KumuluzServer server, EeConfig eeConfig) {
+
+    }
+
+    @Override
+    public void load() {
+        log.info("Initiating Metro");
+    }
+
+    @Override
+    public String getComponentName() {
+        return "JAX-WS";
+    }
+
+    @Override
+    public String getImplementationName() {
+        return "Metro";
+    }
+}
