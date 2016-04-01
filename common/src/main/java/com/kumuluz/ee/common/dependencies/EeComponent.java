fbf047c11697791b108ab419eb2c19e5bfa407a6
Tilen Faganel <tilen.faganel@me.com>
2016-04-01 13:18:49 +0200

    Added the annotation which will serve as the ee component definition

---
+++ common/src/main/java/com/kumuluz/ee/common/dependencies/EeComponent.java
@@ -0,0 +1,15 @@
+package com.kumuluz.ee.common.dependencies;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Tilen Faganel
+ * @since 2.0.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface EeComponent {
+}
