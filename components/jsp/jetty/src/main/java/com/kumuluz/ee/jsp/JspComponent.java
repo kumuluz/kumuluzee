4d1db87e9b11a1fc298cf7c6f4fed30bb1a25977
Tilen Faganel <tilen.faganel@me.com>
2016-04-04 17:04:50 +0200

    Updated Jetty JSP implementation

--- components/jsp/jetty/src/main/java/com/kumuluz/ee/jsp/JspComponent.java
+++ components/jsp/jetty/src/main/java/com/kumuluz/ee/jsp/JspComponent.java
@@ -3,6 +3,7 @@ package com.kumuluz.ee.jsp;
 import com.kumuluz.ee.common.Component;
 import com.kumuluz.ee.common.config.EeConfig;
 import com.kumuluz.ee.common.dependencies.EeComponentDef;
+import com.kumuluz.ee.common.dependencies.EeComponentDependency;
 import com.kumuluz.ee.common.dependencies.EeComponentType;
 import com.kumuluz.ee.common.wrapper.KumuluzServerWrapper;
 
@@ -12,6 +13,7 @@ import java.util.logging.Logger;
  * @author Tilen
  */
 @EeComponentDef(name = "JSP", type = EeComponentType.JSP)
+@EeComponentDependency(value = EeComponentType.EL)
 public class JspComponent implements Component {
 
     private Logger log = Logger.getLogger(JspComponent.class.getSimpleName());
