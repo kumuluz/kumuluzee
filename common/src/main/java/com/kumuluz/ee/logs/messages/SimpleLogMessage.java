2e682aeb38b0ba7f50bfe80ee2700f1be8ec16d7
Rok <povse.rok@gmail.com>
2017-03-27 19:54:12 +0200

    Extended SimpleLogMessage for adding a single field to fields

--- common/src/main/java/com/kumuluz/ee/logs/messages/SimpleLogMessage.java
+++ common/src/main/java/com/kumuluz/ee/logs/messages/SimpleLogMessage.java
@@ -3,6 +3,7 @@
  */
 package com.kumuluz.ee.logs.messages;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -32,5 +33,12 @@ public class SimpleLogMessage implements MethodCallLogMessage, MethodCallExitLog
 
     public void setFields(Map<String, String> fields) {
         this.fields = fields;
+    }
+
+    public void addField(String key, String value) {
+        if (fields == null) {
+            fields = new HashMap();
+        }
+        fields.put(key,value);
     }
 }
