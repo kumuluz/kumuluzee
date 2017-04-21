e8f2cabc2c8848121eff0d84d4d878d8962797a9
Marcos Koch Salvador <marcosks0@gmail.com>
2017-04-21 14:49:15 -0300

    Added JTA support implemented by Narayana

---
+++ components/jta/common/src/main/java/com/kumuluz/ee/jta/common/eclipselink/KumuluzTransactionController.java
@@ -0,0 +1,19 @@
+package com.kumuluz.ee.jta.common.eclipselink;
+
+import com.kumuluz.ee.jta.common.JtaTransactionHolder;
+import org.eclipse.persistence.transaction.JTATransactionController;
+
+import javax.transaction.TransactionManager;
+
+/**
+ * @author Marcos Koch Salvador
+ * @since 2.3.0
+ */
+public class KumuluzTransactionController extends JTATransactionController {
+
+    @Override
+    protected TransactionManager acquireTransactionManager() throws Exception {
+        return JtaTransactionHolder.getInstance().getTransactionManager();
+    }
+
+}
