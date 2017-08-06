784ad85554c97549ba834cbd1b6630b8ef2a2da1
Tilen Faganel <tilen.faganel@me.com>
2017-08-07 00:04:50 +0200

    Added support for advanced DataSource and connection pool configuration options

---
+++ common/src/main/java/com/kumuluz/ee/common/config/DataSourcePoolConfig.java
@@ -0,0 +1,211 @@
+package com.kumuluz.ee.common.config;
+
+public class DataSourcePoolConfig {
+
+    public static class Builder {
+
+        private Boolean autoCommit = true;
+        private Long connectionTimeout = 30000L;
+        private Long idleTimeout = 600000L;
+        private Long maxLifetime = 1800000L;
+        private Integer minIdle;
+        private Integer maxSize = 10;
+        private String name;
+        private Long initializationFailTimeout = 1L;
+        private Boolean isolateInternalQueries = false;
+        private Boolean allowPoolSuspension = false;
+        private Boolean readOnly = false;
+        private Boolean registerMbeans = false;
+        private String connectionInitSql;
+        private String transactionIsolation;
+        private Long validationTimeout = 5000L;
+        private Long leakDetectionThreshold = 0L;
+
+        public Builder autoCommit(Boolean autoCommit) {
+            this.autoCommit = autoCommit;
+            return this;
+        }
+
+        public Builder connectionTimeout(Long connectionTimeout) {
+            this.connectionTimeout = connectionTimeout;
+            return this;
+        }
+
+        public Builder idleTimeout(Long idleTimeout) {
+            this.idleTimeout = idleTimeout;
+            return this;
+        }
+
+        public Builder maxLifetime(Long maxLifetime) {
+            this.maxLifetime = maxLifetime;
+            return this;
+        }
+
+        public Builder minIdle(Integer minIdle) {
+            this.minIdle = minIdle;
+            return this;
+        }
+
+        public Builder maxSize(Integer maxSize) {
+            this.maxSize = maxSize;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder initializationFailTimeout(Long initializationFailTimeout) {
+            this.initializationFailTimeout = initializationFailTimeout;
+            return this;
+        }
+
+        public Builder isolateInternalQueries(Boolean isolateInternalQueries) {
+            this.isolateInternalQueries = isolateInternalQueries;
+            return this;
+        }
+
+        public Builder allowPoolSuspension(Boolean allowPoolSuspension) {
+            this.allowPoolSuspension = allowPoolSuspension;
+            return this;
+        }
+
+        public Builder readOnly(Boolean readOnly) {
+            this.readOnly = readOnly;
+            return this;
+        }
+
+        public Builder registerMbeans(Boolean registerMbeans) {
+            this.registerMbeans = registerMbeans;
+            return this;
+        }
+
+        public Builder connectionInitSql(String connectionInitSql) {
+            this.connectionInitSql = connectionInitSql;
+            return this;
+        }
+
+        public Builder transactionIsolation(String transactionIsolation) {
+            this.transactionIsolation = transactionIsolation;
+            return this;
+        }
+
+        public Builder validationTimeout(Long validationTimeout) {
+            this.validationTimeout = validationTimeout;
+            return this;
+        }
+
+        public Builder leakDetectionThreshold(Long leakDetectionThreshold) {
+            this.leakDetectionThreshold = leakDetectionThreshold;
+            return this;
+        }
+
+        public DataSourcePoolConfig build() {
+
+            DataSourcePoolConfig dataSourcePoolConfig = new DataSourcePoolConfig();
+            dataSourcePoolConfig.autoCommit = autoCommit;
+            dataSourcePoolConfig.connectionTimeout = connectionTimeout;
+            dataSourcePoolConfig.idleTimeout = idleTimeout;
+            dataSourcePoolConfig.maxLifetime = maxLifetime;
+            dataSourcePoolConfig.minIdle = minIdle;
+            dataSourcePoolConfig.maxSize = maxSize;
+            dataSourcePoolConfig.name = name;
+            dataSourcePoolConfig.initializationFailTimeout = initializationFailTimeout;
+            dataSourcePoolConfig.isolateInternalQueries = isolateInternalQueries;
+            dataSourcePoolConfig.allowPoolSuspension = allowPoolSuspension;
+            dataSourcePoolConfig.readOnly = readOnly;
+            dataSourcePoolConfig.registerMbeans = registerMbeans;
+            dataSourcePoolConfig.connectionInitSql = connectionInitSql;
+            dataSourcePoolConfig.transactionIsolation = transactionIsolation;
+            dataSourcePoolConfig.validationTimeout = validationTimeout;
+            dataSourcePoolConfig.leakDetectionThreshold = leakDetectionThreshold;
+
+            return dataSourcePoolConfig;
+        }
+    }
+
+    private Boolean autoCommit;
+    private Long connectionTimeout;
+    private Long idleTimeout;
+    private Long maxLifetime;
+    private Integer minIdle;
+    private Integer maxSize;
+    private String name;
+    private Long initializationFailTimeout;
+    private Boolean isolateInternalQueries;
+    private Boolean allowPoolSuspension;
+    private Boolean readOnly;
+    private Boolean registerMbeans;
+    private String connectionInitSql;
+    private String transactionIsolation;
+    private Long validationTimeout;
+    private Long leakDetectionThreshold;
+
+    private DataSourcePoolConfig() {
+    }
+
+    public Boolean getAutoCommit() {
+        return autoCommit;
+    }
+
+    public Long getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public Long getIdleTimeout() {
+        return idleTimeout;
+    }
+
+    public Long getMaxLifetime() {
+        return maxLifetime;
+    }
+
+    public Integer getMinIdle() {
+        return minIdle;
+    }
+
+    public Integer getMaxSize() {
+        return maxSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getInitializationFailTimeout() {
+        return initializationFailTimeout;
+    }
+
+    public Boolean getIsolateInternalQueries() {
+        return isolateInternalQueries;
+    }
+
+    public Boolean getAllowPoolSuspension() {
+        return allowPoolSuspension;
+    }
+
+    public Boolean getReadOnly() {
+        return readOnly;
+    }
+
+    public Boolean getRegisterMbeans() {
+        return registerMbeans;
+    }
+
+    public String getConnectionInitSql() {
+        return connectionInitSql;
+    }
+
+    public String getTransactionIsolation() {
+        return transactionIsolation;
+    }
+
+    public Long getValidationTimeout() {
+        return validationTimeout;
+    }
+
+    public Long getLeakDetectionThreshold() {
+        return leakDetectionThreshold;
+    }
+}
