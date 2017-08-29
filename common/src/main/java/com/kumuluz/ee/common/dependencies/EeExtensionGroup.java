24a4aa0de4b80f147b1122ac134573a086794e6f
Tilen Faganel <tilen.faganel@me.com>
2017-08-29 16:51:15 +0200

    Added the   official extension group

--- common/src/main/java/com/kumuluz/ee/common/dependencies/EeExtensionGroup.java
+++ common/src/main/java/com/kumuluz/ee/common/dependencies/EeExtensionGroup.java
@@ -9,5 +9,6 @@ public class EeExtensionGroup {
     public final static String SECURITY = "security";
     public final static String FAULT_TOLERANCE = "fault.tolerance";
     public final static String STREAMING = "streaming";
+    public final static String METRICS = "metrics";
     public final static String CORS = "cors";
 }
