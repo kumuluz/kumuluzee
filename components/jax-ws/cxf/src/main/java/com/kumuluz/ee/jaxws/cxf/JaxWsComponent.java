c2fe1ba81704d7dbe2a5c9f47d32b55efbb55307
Gregor <gpor@icloud.com>
2017-09-17 20:53:32 +0200

    added cxf support

---
+++ components/jax-ws/cxf/src/main/java/com/kumuluz/ee/jaxws/cxf/JaxWsComponent.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+package com.kumuluz.ee.jaxws.cxf;
+
+import com.kumuluz.ee.common.Component;
+import com.kumuluz.ee.common.config.EeConfig;
+import com.kumuluz.ee.common.dependencies.EeComponentDef;
+import com.kumuluz.ee.common.dependencies.EeComponentDependency;
+import com.kumuluz.ee.common.dependencies.EeComponentType;
+import com.kumuluz.ee.common.wrapper.KumuluzServerWrapper;
+
+import java.util.logging.Logger;
+
+
+/**
+ * @author Tilen Faganel
+ * @since 2.0.0
+ */
+@EeComponentDef(name = "CXF", type = EeComponentType.JAX_WS)
+@EeComponentDependency(value = EeComponentType.SERVLET)
+public class JaxWsComponent implements Component {
+
+    private Logger log = Logger.getLogger(JaxWsComponent.class.getSimpleName());
+
+    @Override
+    public void init(KumuluzServerWrapper server, EeConfig eeConfig) {
+
+    }
+
+    @Override
+    public void load() {
+
+        log.info("Initiating CXF");
+    }
+}
