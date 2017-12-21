7b59fbe1cac632a358d703863d98a74e50f4bb5b
zvonegit <zvone.gazvoda@gmail.com>
2017-12-21 12:08:11 +0100

    Removed api-spec configuration from plugin.

--- tools/maven-plugin/src/main/java/com/kumuluz/ee/maven/plugin/AbstractPackageMojo.java
+++ tools/maven-plugin/src/main/java/com/kumuluz/ee/maven/plugin/AbstractPackageMojo.java
@@ -17,7 +17,7 @@
  *  out of or in connection with the software or the use or other dealings in the
  *  software. See the License for the specific language governing permissions and
  *  limitations under the License.
-*/
+ */
 package com.kumuluz.ee.maven.plugin;
 
 import org.apache.maven.artifact.Artifact;
@@ -62,7 +62,6 @@ public abstract class AbstractPackageMojo extends AbstractCopyDependenciesMojo {
         finalName = project.getBuild().getFinalName();
 
         copyDependencies("classes/lib");
-        addSwaggerUi();
         unpackDependencies();
         packageJar();
         renameJars();
