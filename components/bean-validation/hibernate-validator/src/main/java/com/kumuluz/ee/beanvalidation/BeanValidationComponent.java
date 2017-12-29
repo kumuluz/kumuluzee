ec23345573e04001d8aeec3d2c63af09b0145d4c
Tilen Faganel <tilen.faganel@me.com>
2017-12-29 16:46:19 +0100

    Updated and optimized the dependency tree of components

--- components/bean-validation/hibernate-validator/src/main/java/com/kumuluz/ee/beanvalidation/BeanValidationComponent.java
+++ components/bean-validation/hibernate-validator/src/main/java/com/kumuluz/ee/beanvalidation/BeanValidationComponent.java
@@ -23,6 +23,7 @@ package com.kumuluz.ee.beanvalidation;
 import com.kumuluz.ee.common.Component;
 import com.kumuluz.ee.common.config.EeConfig;
 import com.kumuluz.ee.common.dependencies.EeComponentDef;
+import com.kumuluz.ee.common.dependencies.EeComponentDependency;
 import com.kumuluz.ee.common.dependencies.EeComponentType;
 import com.kumuluz.ee.common.wrapper.KumuluzServerWrapper;
 
@@ -33,6 +34,7 @@ import java.util.logging.Logger;
  * @since 1.0.0
  */
 @EeComponentDef(name = "Hibernate Validator", type = EeComponentType.BEAN_VALIDATION)
+@EeComponentDependency(value = EeComponentType.EL)
 public class BeanValidationComponent implements Component {
 
     private Logger log = Logger.getLogger(BeanValidationComponent.class.getSimpleName());
