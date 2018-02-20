94136dc381fee69a3affd3ffa153635e55697005
Gregor <gpor@icloud.com>
2018-02-20 18:50:38 +0100

    moved init code from example project, refactoring
    (cherry picked from commit 917484774907bf981c7b26c04f2842acb308a9dd)

--- components/jax-ws/cxf/src/main/java/com/kumuluz/ee/jaxws/cxf/JaxWsComponent.java
+++ components/jax-ws/cxf/src/main/java/com/kumuluz/ee/jaxws/cxf/JaxWsComponent.java
@@ -17,7 +17,7 @@
  *  out of or in connection with the software or the use or other dealings in the
  *  software. See the License for the specific language governing permissions and
  *  limitations under the License.
-*/
+ */
 package com.kumuluz.ee.jaxws.cxf;
 
 import com.kumuluz.ee.common.Component;
